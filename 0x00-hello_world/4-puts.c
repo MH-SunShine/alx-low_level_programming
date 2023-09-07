13cc68281853402f5bb4089dbf4cab0a5d982ec2
Marwa <marwahammamouche.07@gmail.com>
2023-09-07 08:52:20 -0700

    task4 - writing a program that prints :Programming is like building a multilingual puzzle.followed by a new line.

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-
+/**
+* main - prints the string with the put function
+*
+* Description: using the main function
+* this program prints exactly "Programming is like building a multilingual puzzle"
+* Return: 0
+*/
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
