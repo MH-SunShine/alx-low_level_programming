baad890a33499909181a3d7a5d3a77a69dd471da
Marwa <marwahammamouche.07@gmail.com>
2023-09-08 12:40:22 -0700

    task0

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
-* main - prints if the number is postive, zero, or negative
+* main - print if the number is postive, zero, or negative
 *
 * Description: using the main function
 * this program prints "Programming is positive, zero, or negative
--- 0x01-variables_if_else_while/README.md
+++
@@ -1,1 +0,0 @@
-readme file for the second project with C where we are going to discover : varibales - if/else statement and the while loop
