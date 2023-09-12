c2a22f93e99c76884c475ccf507ecf7fa06ac4d6
Marwa <marwahammamouche.07@gmail.com>
2023-09-12 11:50:19 -0700

    task0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0 for successful execution, non-zero for errors
+ */
+
+int main(void)
+{
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
+return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+readme file for the third project , this project is related to nested loops and functions
